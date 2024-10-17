35807028e92f7f4fde4e940264b5eca37865ff74
CeciliaPaney <112904896+CeciliaPaney@users.noreply.github.com>
2024-10-17 12:17:28 +0200

    Identity added

---
+++ AuctionApp/Migrations/20241017094353_Data typ changes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Datatypchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "BidDb",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "AuctionDb",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "AuctionDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "EndTime", "UserId" },
+                values: new object[] { new DateTime(2024, 10, 22, 11, 43, 52, 971, DateTimeKind.Local).AddTicks(5846), "test" });
+
+            migrationBuilder.UpdateData(
+                table: "BidDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "Date", "UserId" },
+                values: new object[] { new DateTime(2024, 10, 17, 11, 43, 52, 971, DateTimeKind.Local).AddTicks(6144), "test2" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "BidDb",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "AuctionDb",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+
+            migrationBuilder.UpdateData(
+                table: "AuctionDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "EndTime", "UserId" },
+                values: new object[] { new DateTime(2024, 10, 20, 13, 7, 23, 410, DateTimeKind.Local).AddTicks(3066), -1 });
+
+            migrationBuilder.UpdateData(
+                table: "BidDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "Date", "UserId" },
+                values: new object[] { new DateTime(2024, 10, 15, 13, 7, 23, 410, DateTimeKind.Local).AddTicks(3291), -2 });
+        }
+    }
+}
---
+++ AuctionApp/Migrations/20241017095301_2Data typ changes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2Datatypchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WinnerId",
+                table: "AuctionDb",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "AuctionDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "EndTime", "WinnerId" },
+                values: new object[] { new DateTime(2024, 10, 22, 11, 53, 0, 837, DateTimeKind.Local).AddTicks(1780), null });
+
+            migrationBuilder.UpdateData(
+                table: "BidDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "Date",
+                value: new DateTime(2024, 10, 17, 11, 53, 0, 837, DateTimeKind.Local).AddTicks(2358));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "WinnerId",
+                table: "AuctionDb",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AuctionDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "EndTime", "WinnerId" },
+                values: new object[] { new DateTime(2024, 10, 22, 11, 43, 52, 971, DateTimeKind.Local).AddTicks(5846), 0 });
+
+            migrationBuilder.UpdateData(
+                table: "BidDb",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "Date",
+                value: new DateTime(2024, 10, 17, 11, 43, 52, 971, DateTimeKind.Local).AddTicks(6144));
+        }
+    }
+}
